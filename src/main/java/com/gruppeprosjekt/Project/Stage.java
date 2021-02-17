80517eab111ffee2faf7bb32ca4809bdd0a05a10
norseninja <74669785+norseninja@users.noreply.github.com>
2021-02-17 16:04:12 +0100

    Started writing code for the ProjectLib class and the TaskMasterClass and all its child classes.

--- src/main/java/com/gruppeprosjekt/Project/Stage.java
+++ src/main/java/com/gruppeprosjekt/Project/Stage.java
@@ -1,4 +1,18 @@
 package com.gruppeprosjekt.Project;
 
-public class Stage {
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ * A single stage in a project,
+ * containing a number of tasks to be finished.
+ */
+public class Stage extends TaskMasterClass {
+
+
+    private ArrayList<Task> tasks;
+    public Stage(String name) {
+        setName(name);
+    }
+
 }
--- src/main/java/com/gruppeprosjekt/Project/TaskMasterClass.java
+++ src/main/java/com/gruppeprosjekt/Project/TaskMasterClass.java
@@ -1,4 +1,56 @@
 package com.gruppeprosjekt.Project;
 
-public class TaskMasterClass {
+import java.time.LocalDate;
+
+public abstract class TaskMasterClass {
+    private String name;
+    private LocalDate deadline;
+    private boolean finished;
+
+    /**
+     * Returns the name of the class.
+     * @return {@code String} name of the class.
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Sets the name of the task.
+     */
+    protected void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the deadline of the task.
+     * @return {@code LocalDate} deadline of the task.
+     */
+    public LocalDate getDeadline() {
+        return this.deadline;
+    }
+
+    /**
+     * Assigns a deadline for the task.
+     * @param date {@code LocalDate} deadline of the task.
+     */
+    public void setDeadline(LocalDate date) {
+        this.deadline = date;
+    }
+
+    /**
+     * Is this task finished?
+     * @return true if finished, false if not.
+     */
+    public boolean isFinished() {
+        return this.finished;
+    }
+
+    /**
+     * Sets the tasks finished state to true or false
+     * @param finished {@code boolean} finished state of the project.
+     */
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
 }
