10c98c967ddf4eb3f4d7b1f2a54febff458e86b7
Haukaun <haakonfs@hotmail.com>
2021-03-09 13:16:56 +0100

    Maven oppsett rett.

--- src/main/java/com/gruppeprosjekt/Util/App.java
+++ src/main/java/com/gruppeprosjekt/Util/App.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 
 /**
  * JavaFX App
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.gruppeprosjekt {
     requires javafx.controls;
+    requires javafx.fxml;
     exports com.gruppeprosjekt.Util;
 }
