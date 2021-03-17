d2a187123c8297899eb463f0889c882bfe7679ad
Espen Otlo <espenotlo@gmail.com>
2021-03-17 13:02:00 +0000

    Upload New File

---
+++ src/main/java/com/snikkergutane/user/ProjectLead.java
@@ -0,0 +1,30 @@
+package com.snikkergutane.user;
+
+import java.util.ArrayList;
+
+
+/**
+ * The project lead holds the primary responsibility of a project.
+ * Project Lead can view all projects,
+ * and edit and mark as finished the project(s) they are assigned to.
+ */
+public class ProjectLead extends UserType {
+
+    public ProjectLead() {
+        this.name = "Project Lead";
+        this.permissions = new ArrayList<>();
+        this.permissions.add("readAll");
+        this.permissions.add("editAssignedProject");
+        this.permissions.add("finishAssignedProject");
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public ArrayList<String> getPermissions() {
+        return this.permissions;
+    }
+}
