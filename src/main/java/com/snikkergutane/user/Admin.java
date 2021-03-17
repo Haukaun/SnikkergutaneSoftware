7487f10a29dec1edefc57ff8df24a84873b7edcf
Espen Otlo <espenotlo@gmail.com>
2021-03-17 13:01:34 +0000

    Upload New File

---
+++ src/main/java/com/snikkergutane/user/Admin.java
@@ -0,0 +1,28 @@
+package com.snikkergutane.user;
+
+import java.util.ArrayList;
+
+/**
+ * The Admin has full access to all the systems functions;
+ * they can create, edit and delete projects, and add or edit
+ * comments, dates and descriptions of tasks.
+ * They can also edit the permissions of other users.
+ */
+public class Admin extends UserType {
+
+    public Admin() {
+        this.name = "Admin";
+        this.permissions = new ArrayList<>();
+        this.permissions.add("all");
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public ArrayList<String> getPermissions() {
+        return this.permissions;
+    }
+}
