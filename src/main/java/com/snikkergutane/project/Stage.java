0300d436e4444f64d87167410f7697c769c411ee
Espen Otlo <espenotlo@gmail.com>
2021-03-17 12:59:56 +0000

    Upload New File

---
+++ src/main/java/com/snikkergutane/project/Stage.java
@@ -0,0 +1,19 @@
+package com.snikkergutane.project;
+
+import com.snikkergutane.project.task.Task;
+
+import java.util.ArrayList;
+
+/**
+ * A single stage in a project,
+ * containing a number of tasks to be finished.
+ */
+public class Stage extends TaskMasterClass {
+
+    private ArrayList<Task> tasks;
+
+    public Stage(String name) {
+        setName(name);
+    }
+
+}
