519e856de5a9719173ab835accafa7de51406591
qiao-ntnu <qiaoduos@stud.ntnu.no>
2021-03-26 19:48:36 +0100

    Add gitignore file and positive/negative test to method

--- src/test/java/com/snikkergutane/project/ProjectTest.java
+++ src/test/java/com/snikkergutane/project/ProjectTest.java
@@ -6,18 +6,38 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @author Qiaoduo Shi
+ * @version 2020.03.26
+ */
 class ProjectTest {
-
+    private Project project;
+    private Stage stage1;
+    private Stage stage2;
     @BeforeEach
     void setUp() {
+        this.project = new Project("Project1");
+        this.stage1 = new Stage("Stage1");
+        this.stage2 = new Stage("Stage2");
+        this.project.addStage(stage1);
+        this.project.addStage(stage2);
     }
 
     @AfterEach
     void tearDown() {
     }
 
+    /**
+     * Test to get a stage from a project.
+     */
     @Test
-    void getStage() {
+    void testGetStage() {
+        //positive test
+        assertEquals(this.stage1, project.getStage("Stage1"));
+        assertEquals(null,project.getStage("Nothing"));
+        //assertEquals("Stage1", project.getStage("Stage1").getName());
+        //negative test with null input.
+        assertEquals(null, project.getStage(null));
     }
 
     @Test
